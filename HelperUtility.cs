c915e5dd36e4378ffb7a3e65cbf7065062c11964
vishnukumarps <vishnukumar29518@gmail.com>
2020-05-25 08:49:34 +0530

    Static and helper

---
+++ HelperUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teaching
+{
+   public class HelperUtility
+    {
+
+        public void PrintHello10()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,12 +2,21 @@
 
 namespace Teaching
 {
-    class Program
-    {
-        using System;
 
-namespace Day_2
+
+    namespace Day_2
     {
+
+        class Student
+        {
+            public int RollNo;
+             public int M1;
+            public int M2;
+            public string Name;
+            
+            public  static string SchoolName;
+
+        }
         class Calc
         {
             public void Hello()// with out return type with out param
@@ -29,6 +38,12 @@ namespace Day_2
                 Console.WriteLine("Sum =" + (x + y));
             }// with pram no return 
 
+            public int Sub(int x, int y)
+            {
+
+
+                return x - y;
+            }
 
 
 
@@ -36,9 +51,11 @@ namespace Day_2
 
         class Program
         {
-            static void Main(string[] args)
+             static void Main(string[] args)
             {
 
+                Program objj = new Program();
+               
                 Calc obj = new Calc();
 
                 obj.Hello();
@@ -46,10 +63,36 @@ namespace Day_2
                 MyNumber = obj.getMeANumber();
                 Console.WriteLine("My Random num=" + MyNumber);
                 obj.AddTwoNum(12, 12);
+
+                int sub = obj.Sub(100, 90);
+                Console.WriteLine("Sub" + sub);
+
+
+
+                HelperUtility obj2 = new HelperUtility();
+                obj2.PrintHello10();
+
+
+                Student s1 = new Student();
+                s1.Name = "Vishnu";
+                s1.M1 = 20;
+
+                Student s2 = new Student();
+                s2.Name = "Manu";
+             
+                
+                Student.SchoolName = "NSSHSS";
+
                 Console.ReadLine();
+            }
+
+            void manu()
+            {
+
             }
         }
     }
+}
 
-}
-}
+
+  
