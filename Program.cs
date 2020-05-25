710b1a513e7980b80f577ec7dc21f88c7533f284
vishnukumarps <vishnukumar29518@gmail.com>
2020-05-25 08:11:05 +0530

    Day2

--- Program.cs
+++ Program.cs
@@ -4,10 +4,52 @@ namespace Teaching
 {
     class Program
     {
-        static void Main(string[] args)
+        using System;
+
+namespace Day_2
+    {
+        class Calc
         {
-            Console.WriteLine("Hello World!");
-            hju8uu
+            public void Hello()// with out return type with out param
+            {
+                Console.WriteLine("Hello");
+            }
+
+            public int getMeANumber() // wih return type with out pram
+            {
+                Random ran = new Random();
+
+                return ran.Next(100);
+
+            }
+
+
+            public void AddTwoNum(int x, int y)
+            {
+                Console.WriteLine("Sum =" + (x + y));
+            }// with pram no return 
+
+
+
+
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Calc obj = new Calc();
+
+                obj.Hello();
+                int MyNumber;
+                MyNumber = obj.getMeANumber();
+                Console.WriteLine("My Random num=" + MyNumber);
+                obj.AddTwoNum(12, 12);
+                Console.ReadLine();
+            }
         }
     }
+
+}
 }
