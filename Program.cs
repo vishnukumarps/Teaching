f9c71d7d80aad8321743f958fbe7de9b52ed6e1d
vishnukumarps <vishnukumar29518@gmail.com>
2020-05-25 08:07:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teaching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
