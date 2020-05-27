fa010b1da4423b16f7ed163f18dab0d2e0a53511
vishnukumarps <vishnukumar29518@gmail.com>
2020-05-27 07:45:26 +0530

    Day_3

---
+++ Array.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teaching
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+         
+            int[] a = new int[10];
+          
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = i * 10;
+
+            }
+            Console.WriteLine("Array Elements are");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.WriteLine("______________________________");
+
+            Console.WriteLine("Array Elements are");
+            for (int i = a.Length-1; i>=0; i--)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teaching
+{
+    class Day3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            Console.ReadLine();
+        }
+    }
+}
