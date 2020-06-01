a9fd534927d92029afca29830d56ecba7b9b5253
vishnukumarps <vishnukumar29518@gmail.com>
2020-06-01 19:55:41 +0530

    inheritance 2

---
+++ Hierrarical.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teaching
+{
+    class Father
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello From father class");
+        }
+        public string HomeName = "xyz home";
+        public string Address = "xyz Address";
+    }
+    class Son : Father
+    {
+       public  void HelloFromSon()
+        {
+            Console.WriteLine("Hello From son class");
+
+        }
+
+        void Car()
+        {
+
+        }
+
+    }
+
+    class Daughter: Father
+    {
+       public void HelloFromDaughter()
+        {
+            Console.WriteLine("Hello From Daughter class");
+
+        }
+        void scooter()
+        {
+
+        }
+    }
+    class Hierrarical
+    {
+        static void Main(string[] args)
+        {
+            Son o = new Son();
+            o.HelloFromSon();
+            o.Hello();
+
+            Daughter ob1 = new Daughter();
+            ob1.Hello();
+            ob1.HelloFromDaughter();
+        }
+    }
+}
