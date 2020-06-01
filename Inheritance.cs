0d130be5478899e4d0bc186f3b3f84c5d210e23d
vishnukumarps <vishnukumar29518@gmail.com>
2020-06-01 19:41:35 +0530

    Multilevel

---
+++ Inheritance.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teaching
+{
+
+    // multilevel
+    class A
+    {
+      public void Hello()
+        {
+            Console.WriteLine("hello");
+        }
+       public  A()
+        {
+
+        }
+       
+    }
+    class B:A
+    {
+        public B()
+        {
+          
+        }
+        public void Hai()
+        {
+            Console.WriteLine("Hai");
+        }
+      
+    }
+    class C:B
+    {
+       public void Test()
+        {
+            Console.WriteLine("Test");
+        }
+      public  C()
+        {
+
+        }
+
+    }
+
+    class Inheritance
+    {
+        static void Main(string[] args)
+        {
+            C o = new C();// creating an object of  Subclass
+            o.Hai();
+            o.Test();
+            o.Hello();
+
+
+
+
+            A o1 = new B();
+            B o2 = new C();
+           
+        }
+    }
+}
